5105a7274bd5cf8144458d42a8feb10529cc7876
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2023-03-15 15:30:42 -0600

    Update EP_CallingSequences.py

--- EP_CallingSequences.py
+++ EP_CallingSequences.py
@@ -18,19 +18,10 @@ def timeStepHandler(state):
     _converge = 2 > abs(accumulated_time - 30 * 60)
     print(f'HVAC detecting convergence,{_converge},'
           f'curr_sim_time_in_seconds: {curr_sim_time_in_seconds},'
-          f'eplastcalltime[threadName]: {eplastcalltimexl[threadName]}')
+          f'eplastcalltime[threadName]: {eplastcalltime[threadName]}')
     if not _converge:
         return
     eplastcalltime[threadName] = curr_sim_time_in_seconds
-    with cond_mid:
-        _bool = ep_to_get_waste_check(shared_dict, threadName)
-        while not _bool:
-            cond_mid.wait()
-            _bool = ep_to_get_waste_check(shared_dict, threadName)
-        shared_dict[threadName]['wasteHeat'] = 300 + random.randint(1, 10)
-        shared_dict[threadName]['status'] = 'to_upload_wasteHeat'
-    with cond_mid:
-        cond_mid.notify_all()
 def overwrite_ep_weather(state):
     global call_thread, eplastcalltime_over, shared_dict
     warm_up = ep_api.exchange.warmup_flag(state)
@@ -50,17 +41,6 @@ def overwrite_ep_weather(state):
             return
         eplastcalltime_over[threadName] = curr_sim_time_in_seconds
 
-        with cond_mid:
-            _bool = ep_weather_overwrite_check(shared_dict, threadName)
-            while not _bool:
-                cond_mid.wait()
-                _bool = ep_weather_overwrite_check(shared_dict, threadName)
-            print(f'EP {threadName} call time: {curr_sim_time_in_seconds},'
-                  f'weatherInfo: {weatherInfo},')
-            shared_dict[threadName]['time'] = curr_sim_time_in_seconds
-            shared_dict[threadName]['status'] = 'to_get_wasteHeat'
-        with cond_mid:
-            cond_mid.notify_all()
 
 def one_idf_run(name):
     state = ep_api.state_manager.new_state()
@@ -109,71 +89,12 @@ def Call_EP():
         shared_dict[_tmpEPName] = _tmpDict
         thread_idf = threading.Thread(target=one_idf_run, name = _tmpEPName, args=(_tmpEPName,))
         thread_idf.start()
-def vcwg_generate_weather_check(dict):
-    for key, value in dict.items():
-        if key == 'vcwg_time' or key == 'weatherInfo':
-            continue
-        if value['status'] != 'to_generate_weather':
-            # print(f'vcwg_generate_weather_check: {dict}')
-            return False
-    return True
-def vcwg_download_wasteHeat_check(dict):
-    for key, value in dict.items():
-        if key == 'vcwg_time' or key == 'weatherInfo':
-            continue
-        if value['status'] != 'to_upload_wasteHeat':
-            # print(f'vcwg_download_wasteHeat_check: {dict}')
-            return False
-    return True
-def ep_weather_overwrite_check(dict, _threadName):
-    if dict[_threadName]['status'] == 'to_overwrite_weather':
-        return True
-    else:
-        # print(f'ep_weather_overwrite_check: {dict}')
-        return False
-def ep_to_get_waste_check(dict, _threadName):
-    if dict[_threadName]['status'] == 'to_get_wasteHeat':
-        return True
-    else:
-        # print(f'ep_to_get_waste_check: {dict}')
-        return False
+
 def run_vcwg():
     Call_EP()
     global shared_dict
     shared_dict['vcwg_time'] = 0
     shared_dict['weatherInfo'] = 25 + random.randint(1, 10)
-    while True:
-        with cond_mid:
-            _bool = vcwg_generate_weather_check(shared_dict)
-            while not _bool:
-                cond_mid.wait()
-                _bool = vcwg_generate_weather_check(shared_dict)
-            # cond_mid.wait_for(lambda: vcwg_generate_weather_check(shared_dict))
-            shared_dict['vcwg_time'] += 30 * 60
-            shared_dict['weatherInfo'] = 25 + random.randint(1, 10)
-            for key, value in shared_dict.items():
-                if key != 'vcwg_time' and key != 'weatherInfo':
-                    value['status'] = 'to_overwrite_weather'
-            print(f'VCWG to generate weather,'
-                  f'shared_dict: {shared_dict}')
-        with cond_mid:
-            cond_mid.notify_all()
-
-        with cond_mid:
-            _bool = vcwg_download_wasteHeat_check(shared_dict)
-            while not _bool:
-                cond_mid.wait()
-                _bool = vcwg_download_wasteHeat_check(shared_dict)
-            # cond_mid.wait_for(lambda: vcwg_download_wasteHeat_check(shared_dict))
-            for key, value in shared_dict.items():
-                if key != 'vcwg_time' and key != 'weatherInfo':
-                    value['wasteHeat'] = -1
-                    value['status'] = 'to_generate_weather'
-            print(f'VCWG to download wasteHeat,'
-                  f'shared_dict: {shared_dict}')
-        with cond_mid:
-            cond_mid.notify_all()
-
 
 if __name__ == '__main__':
     run_vcwg()
